bc2a9efc8ffde1926fabfe668f0e5d8b931bc6e7
xdoer123 <110903150+xdoer123@users.noreply.github.com>
2023-05-05 09:11:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const whatsapp_1 = require("./helper/whatsapp");
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.get('/', whatsapp_1.getStatus);
